45b12be0bdd6777bc6eb19d35cc6cb3cd77308d3
Adrian Orłów <aorlow@mail.com>
2019-09-26 21:33:50 +0200

    Change: Footer redesign

--- src/components/Footer/FooterView.tsx
+++ src/components/Footer/FooterView.tsx
@@ -1,39 +1,54 @@
 import * as React from 'react';
+import { NavLink } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faHeart, faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon as FaIcon } from '@fortawesome/react-fontawesome';
+
 import './Footer.scss';
+import { ReactComponent as Logo } from 'assets/vectors/logo-grey.svg'
 
 library.add(faHeart, faCoffee);
 
 const Footer: React.FC = () => (
   <footer className="home__footer">
-    <div className="home__footer__inner">
-      <span>
-          Made with
-        {' '}
-        <FaIcon icon={faHeart} />
-        {' '}
-by
-        {' '}
+    <section className="home__footer__section">
+      <div  className="home__footer__list">
+        <Logo className="home__footer__logo" />
+      </div>
+      <ul className="home__footer__list">
+        <li>
+          <NavLink to="/privacy">Prywatność</NavLink>
+        </li>
+        <li>
+          <a
+            href="https://facebook.com/myPoliticsTest"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Facebook</a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/myPolitics"
+            target="_blank"
+            rel="noopener noreferrer"
+          >GitHub</a>
+        </li>
+      </ul>
+    </section>
+    <section className="home__footer__section">
+      <span className="home__footer__copyright">
+        {'Made with '}
+        <FaIcon icon={faHeart} className="heart" />
+        {' by '}
         <a
           href="https://orlow.me"
           target="_blank"
           rel="noopener noreferrer"
         >
-Adrian Orłów
+        {'Adrian Orłów'}
         </a>
       </span>
-      <span>
-        <a
-          href="https://github.com/myPolitics"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-GitHub
-        </a>
-      </span>
-    </div>
+    </section>
   </footer>
 );
 
