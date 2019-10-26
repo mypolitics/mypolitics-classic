d353d555597af02c36a3421a49998c865d225c96
Adrian Orłów <aorlow@mail.com>
2019-10-26 22:10:30 +0200

    Add: backward compatibility for Results links

---
+++ src/components/App/AppContainer.ts
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import AppView from './AppView';
+
+class App extends React.Component<any> {
+  componentDidMount() {
+    const url = window.location.href;
+
+    if (url.includes('/#/results/')) {
+      const resultsId = url.split('/#/results/')[1];
+
+      window.location.hash = '';
+      window.location.pathname = `/results/${resultsId}`;
+    }
+  }
+
+  render = () => AppView({})
+}
+
+export default App;
--- src/components/App/index.ts
+++ src/components/App/index.ts
@@ -1,1 +1,1 @@
-export { default } from './AppView';
+export { default } from './AppContainer';
