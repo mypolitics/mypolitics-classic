6df04a8b9117b99bf1970ae1499cea254dcc1153
Adrian Orłów <adrian@orlow.me>
2020-05-23 22:32:37 +0200

    Update API URL

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,7 +1,7 @@
 import quizService from './quiz';
 import resultsService from './results';
 
-export const API_URL = 'https://mypolitics-api.orlow.me/';
+export const API_URL = 'https://api.mypolitics.pl/';
 export const DEAFULT_OPTS = {
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
