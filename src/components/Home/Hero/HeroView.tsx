52882f4f83dfc4774572da16f906f78620393435
Adrian Orłów <adrian@orlow.me>
2020-07-22 00:26:49 +0200

    Update: home hero section

--- src/components/Home/Hero/HeroView.tsx
+++ src/components/Home/Hero/HeroView.tsx
@@ -4,12 +4,10 @@ import './Hero.scss';
 
 const Hero: React.FC = () => (
   <section className="home__section home__hero">
-    <img className="home__hero__image" src="/hero.jpg" />
+    <img className="home__hero__image" src="/hero.jpg" alt="hero image" />
     <div className="home__hero__overlay" />
     <div className="home__hero__container">
-      <h1 className="home__hero__title">
-        Najbardziej zaawansowany polski test polityczny
-      </h1>
+      <img className="home__hero__title" src="/vectors/logotype.svg" />
       <Link className="home__hero__btn" to="/quiz">
           ROZPOCZNIJ
       </Link>
