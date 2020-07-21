0eccc4f6011920b1866c01764fd94e3d115a6a47
Adrian Orłów <adrian@orlow.me>
2020-07-21 23:40:19 +0200

    Change: remove ideologies section from home

--- src/components/Home/Hero/HeroView.tsx
+++ src/components/Home/Hero/HeroView.tsx
@@ -4,6 +4,8 @@ import './Hero.scss';
 
 const Hero: React.FC = () => (
   <section className="home__section home__hero">
+    <img className="home__hero__image" src="/hero.jpg" />
+    <div className="home__hero__overlay" />
     <div className="home__hero__container">
       <h1 className="home__hero__title">
         Najbardziej zaawansowany polski test polityczny
--- src/components/Home/HomeView.tsx
+++ src/components/Home/HomeView.tsx
@@ -3,14 +3,12 @@ import Footer from 'components/Footer';
 import Hero from './Hero';
 import Info from './Info';
 import Stats from './Stats';
-import Ideologies from './Ideologies';
 import './Home.scss';
 
 const Home: React.FC = () => (
   <main className="home">
     <Hero />
     <Info />
-    <Ideologies />
     <Stats />
     <Footer />
   </main>
